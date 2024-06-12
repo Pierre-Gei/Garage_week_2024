37d89cf5f9d814da476ed40e0a5f5c8649b2bd69
Pierre-Gei <pierregeiguer@gmail.com>
2024-06-12 14:12:56 +0200

    Starting backend

---
+++ lib/models/entrepriseModel.dart
@@ -0,0 +1,13 @@
+class entreprise{
+  final String id;
+  final String nom;
+  final String adresse;
+  final String ville;
+
+  entreprise({
+    required this.id,
+    required this.nom,
+    required this.adresse,
+    required this.ville,
+  });
+}
